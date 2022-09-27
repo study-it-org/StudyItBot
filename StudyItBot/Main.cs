722dcdbf8d62a3bf772d332477e66ec49aabfdc0
Tim <tigga01@online.de>
2022-09-27 20:09:12 +0200

    Getting Started

---
+++ StudyItBot/Main.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.WebSocket;
+using NLog;
+
+namespace StudyItBot;
+
+public class Program
+{
+    private readonly DiscordSocketClient _discordSocketClient = new();
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    public static void Main(string[] args)
+    {
+        new Program().MainAsync().GetAwaiter().GetResult();
+    }
+
+    private async Task MainAsync()
+    {
+        var token = Environment.GetEnvironmentVariable("token");
+        if (token == null)
+        {
+            _logger.Fatal("No token provided in the \"token\" environment variable");
+            return;
+        }
+
+        await _discordSocketClient.LoginAsync(TokenType.Bot, "");
+        await Task.Delay(-1);
+    }
+}
